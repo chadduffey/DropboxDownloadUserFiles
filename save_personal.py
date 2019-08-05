70cfce7315272224e0419e4dd9cb0b4ed9ea38e8
Chad <chad@Chads-MacBook-Air.local>
2019-08-05 10:18:24 -1000

    csv output

--- save_personal.py
+++ save_personal.py
@@ -1,5 +1,6 @@
 # The existing script was for DBX for Business. Creating single DBX export. 
 
+import csv
 import dropbox
 import os.path
 
@@ -20,16 +21,18 @@ def confirm_api():
 	print("Logged in to account: {}".format(current.name.display_name))
 
 
-def list_folder(folder='', recursive=False):
-	folder_list = dbx.files_list_folder(folder).entries
-	for entry in folder_list:
+def get_all_files(folder='', recursive=False):
+	all_files = []
+	folder_content = dbx.files_list_folder(folder).entries
+	for entry in folder_content:
 		if type(entry) is dropbox.files.FolderMetadata:
 			print("{}".format(entry.path_lower))
 			if recursive:
-				list_folder(folder=entry.path_lower, recursive=recursive)
+				get_all_files(folder=entry.path_lower, recursive=recursive)
 		elif type(entry) is dropbox.files.FileMetadata:
 			print("{} [{}]".format(entry.path_lower, entry.content_hash))
-	return folder_list
+			all_files.append(entry)
+	return all_files
 
 
 def save_file(dbx_path, local_path):
@@ -40,15 +43,22 @@ def save_file(dbx_path, local_path):
 
 
 def save_all_files(folder=""):
-	content_list = list_folder(folder)
+	content_list = get_all_files(folder)
 	for item in content_list:
 		if type(item) is dropbox.files.FileMetadata and item.is_downloadable:
 			if not path.exists(save_location + item.path_lower):
 				print("Downloading {}".format(item.name))
-				save_file(item.path_lower, save_location + item.path_lower)	
+				save_file(item.path_lower, save_location + item.path_lower)
+
+
+def save_csv(file_list, destination="junk"):
+	with open(destination + "/out.csv", 'w', newline='') as of:
+		csvwriter = csv.writer(of, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		for f in file_list:
+			csvwriter.writerow([f.path_lower, f.content_hash])
 
 
 dbx = dropbox.Dropbox(get_token())
 confirm_api()
-list_folder(recursive=True)
-save_all_files()
+all_files = get_all_files(recursive=True)
+save_csv(all_files)
